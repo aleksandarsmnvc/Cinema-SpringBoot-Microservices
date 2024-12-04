43ac0a13cdef3c216bbc47bcdac8c31bfe1c3db8
Aleksandar Simeunovic <aleksandarsmnvc@gmail.com>
2024-12-04 15:23:54 +0100

    movie module deleated

--- movie-service/src/main/java/com/aleksandar_simeunovic/movie_service/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aleksandar_simeunovic.movie_service.repository;
-
-import com.aleksandar_simeunovic.movie_service.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie,Integer> {
-}
