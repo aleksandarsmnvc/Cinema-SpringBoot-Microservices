3d631f301d66cb7d4f8d6bc6ed5bde8654c16d1f
Aleksandar Simeunovic <aleksandarsmnvc@gmail.com>
2024-12-04 14:42:52 +0100

    Eureka corrections

--- eureka-server/src/main/java/com/aleksandar_simeunovic/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/aleksandar_simeunovic/eureka_server/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.aleksandar_simeunovic.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
