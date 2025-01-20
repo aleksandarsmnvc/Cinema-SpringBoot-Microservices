9fff22eba2e13cd79a319f35c0e50ed37186a825
Aleksandar Simeunovic <aleksandarsmnvc@gmail.com>
2025-01-20 17:36:49 +0100

    Added configurations.properties file.

---
+++ file-service/src/test/java/com/aleksandar_simeunovic/file_service/FileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksandar_simeunovic.file_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
