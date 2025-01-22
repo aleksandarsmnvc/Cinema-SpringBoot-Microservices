b529431e937301cc7adfce855e793e72a5a7589b
Aleksandar Simeunovic <aleksandarsmnvc@gmail.com>
2025-01-22 06:23:34 +0100

    Gateway routes and filter configured.

---
+++ gateway/src/main/java/com/aleksandar_simeunovic/gateway/filter/JwtUtil.java
@@ -0,0 +1,20 @@
+package com.aleksandar_simeunovic.gateway.filter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
